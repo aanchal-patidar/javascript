11a162dfe3461910b3bcf758d52a58267ce95355
Aanchal Patidar <146761680+aanchal-patidar@users.noreply.github.com>
2024-05-22 17:54:13 +0000

    Data type conversion

---
+++ 01_basics/03_dataTypeConversion.js
@@ -0,0 +1,37 @@
+let score = 1;
+
+
+//conversion of other to number
+/* 
+"33" => 33
+"33abc" => NaN
+"boolean" => true - 1/ false - 0
+null => 0
+undefined => NaN
+*/
+console.log(score)
+console.log(typeof(score))
+// let stringtoNum = Number(score);
+
+
+
+/*
+converson of other datatype to string
+33 => "33"
+null => null
+undefined => undefined
+ */
+// let otherToString = String(score);
+// console.log(otherToString)
+
+
+/*
+Conversion in boolean
+1=> true
+0=> false
+""=> false
+"abhd" => true
+*/
+let otherToboolean = Boolean(score);
+console.log(otherToboolean)
+
