be81aef462b4d85fe42b0976ba91e09199d4fe76
Aanchal Patidar <146761680+aanchal-patidar@users.noreply.github.com>
2024-05-23 05:25:16 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,10 @@
+console.log(2==2)
+console.log(2 >= 1)
+console.log(2 <= 1)
+console.log(2 != 1)
+
+
+// comparision wth other data type
+
+console.log("2" == 2)
+console.log("2"===2) //strict rule for comparing the data as well as type
