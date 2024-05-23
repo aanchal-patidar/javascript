54834e77366687f05b28f53843cbadb38bad9028
Aanchal Patidar <146761680+aanchal-patidar@users.noreply.github.com>
2024-05-23 17:49:33 +0000

    Strings in js

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,20 @@
+let name = "Aanchal";
+
+let count = 20;
+
+console.log(`My name is ${name} and count is ${count}`);
+
+const newName = new String('Aanchal-P')
+
+console.log(newName[0])
+console.log(newName.__proto__);
+
+console.log(newName.toLowerCase)
+console.log(newName.charAt(2))
+console.log(newName.length)
+console.log(newName.indexOf('n'))
+console.log(newName.substring(0,5)) // it can consider only positive arguments
+console.log(newName.slice(-2,7))  // it can take pos and neg both type of arguments
+const anotherName = '   Payal';
+console.log(anotherName);
+console.log(anotherName.trim())
