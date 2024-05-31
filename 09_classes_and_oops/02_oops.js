40f075a275dc5205e77be0d1b2f325c4f0e5d85b
Aanchal Patidar <146761680+aanchal-patidar@users.noreply.github.com>
2024-05-31 07:04:31 +0000

    constructor function

---
+++ 09_classes_and_oops/02_oops.js
@@ -0,0 +1,30 @@
+// object literal
+
+const user = {
+    username : 'Aanchal',
+    isLoggedIn : true,
+    loggedOut : false,
+    greetings : ()=> {
+        // console.log(`greeting to the ${this.username}`);
+        console.log(this);              // print all information about this object
+    }
+
+}
+console.log(this);              // print empty object becasue it is available in the node environment
+// console.log(user.username);
+// console.log(user.greetings());
+
+// using constructor literral
+function Myfunction(usernamne,isLoggedIn)
+{
+    this.username = usernamne
+    this.isLoggedIn = isLoggedIn
+    return this;
+
+}
+
+// new keyword is create  new instance for different parameters
+const details1 = new Myfunction("Aanchal",true)
+const details2 = new Myfunction("Payal",false)
+console.log(details1);
+console.log(details2);
